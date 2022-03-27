7078ed2d7d04af2ba417072d027153530dc8c6e7
andrey_silivanov <andrey.silivanovv@gmail.com>
2022-03-27 17:57:45 +0300

    base html

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="container-fluid">
+
+      <main-block/>
+
+    <!-- page-body-wrapper ends -->
+  </div>
+</template>
+
+<script>
+
+import MainBlock from "@/components/MainBlock";
+
+export default {
+  name: 'App',
+  components: {
+    MainBlock
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "src/assets/scss/style.scss";
+</style>
---
+++ frontend/src/components/MainBlock.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <div class="clearfix content-wrapper">
+    <div class="row">
+
+    <people-list :show="true"/>
+    <chat-component/>
+    </div>
+
+  </div> <!-- end container -->
+
+  </div>
+
+
+</template>
+
+<script>
+
+import PeopleList from "@/components/PeopleList";
+import ChatComponent from "@/components/ChatComponent";
+
+export default {
+  name: "MainBlock",
+  components: {
+    PeopleList,
+    ChatComponent
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
